fb7cdd734a67e1b215c1acf1faa1457786926c1f
mina <28566705+mina9999@users.noreply.github.com>
2021-06-12 14:20:49 +0430

    Delete invite.js

--- invite.js
+++
@@ -1,9 +0,0 @@
-const util = require("../util");
-
-module.exports = {
-    name: "invite",
-    aliases: ["link"],
-    exec: (msg) => {
-        msg.channel.send(util.embed().setDescription("💜💜 | [Click Here](https://discord.com/oauth2/authorize?client_id=428900318852546560&scope=bot&permissions=66186303) To invite Rose bot to your server💜💜"));
-    }
-};
